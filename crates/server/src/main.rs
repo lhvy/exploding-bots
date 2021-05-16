3e2918a22b892840df6687b3d52b2f7dca4b93cf
lhvy <me@lhvy.dev>
2021-05-16 22:46:52 +1000

    Add start of server

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::BufReader, net::TcpListener};
+use types::User;
+use uuid::Uuid;
+
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:2021")?;
+    let mut users = Vec::new();
+    for stream in listener.incoming() {
+        let stream = stream?;
+        users.push(User {
+            id: Uuid::new_v4(),
+            name: jsonl::read(BufReader::new(stream))?,
+        });
+    }
+    Ok(())
 }
