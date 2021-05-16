2a2f5f4e81227c82feb18fd06ab66a8e4982ea2e
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-05-17 08:19:23 +1000

    Make server multithreaded

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,16 +1,38 @@
-use std::{io::BufReader, net::TcpListener};
+use parking_lot::Mutex;
+use std::io::BufReader;
+use std::net::{TcpListener, TcpStream};
+use std::sync::Arc;
+use std::thread;
 use types::User;
 use uuid::Uuid;
 
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:2021")?;
-    let mut users = Vec::new();
+    let users = Arc::new(Mutex::new(Vec::new()));
+
     for stream in listener.incoming() {
         let stream = stream?;
-        users.push(User {
-            id: Uuid::new_v4(),
-            name: jsonl::read(BufReader::new(stream))?,
+        let users = Arc::clone(&users);
+
+        thread::spawn(|| {
+            if let Err(e) = handle_connection(stream, users) {
+                eprintln!("Error: {:#?}", e);
+            }
         });
     }
+
+    Ok(())
+}
+
+fn handle_connection(stream: TcpStream, users: Arc<Mutex<Vec<User>>>) -> anyhow::Result<()> {
+    let name = jsonl::read(BufReader::new(stream))?;
+
+    users.lock().push(User {
+        id: Uuid::new_v4(),
+        name,
+    });
+
+    dbg!(&users);
+
     Ok(())
 }
